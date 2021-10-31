f0ac19f1a7c0f33269ec5d28854e3a8415a7e9aa
pankaj970 <93472499+pankaj970@users.noreply.github.com>
2021-10-31 16:29:57 +0530

    Create g.cpp

---
+++ g.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char *name = "Deep";
+
+    int i;
+    for (i = 0; name[i] != '\0'; i++)
+    {
+    }
+
+    cout << i << endl;
+
+    return 0;
+}
